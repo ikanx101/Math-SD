9a542f3a4b7bd42e903addc21f5039a24d85d73a
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2021-09-15 17:34:21 +0700

    Update main.R

--- main.R
+++ main.R
@@ -12,9 +12,10 @@ clc()
 
 cat("===================================================\n")
 cat("            GENERATOR SOAL BERHITUNG\n")
-cat("			 versi 1.0\n")
+cat("		        versi 1.0\n")
+cat("\nSelamat datang di program ini. Program ini bertujuan untuk membuat soal hitung-hitungan untuk anak SD kelas 2 - 4. Kamu bisa menentukan berapa banyak soal penambahan, pengurangan, perkalian, dan pembagian yang akan dikerjakan. Waktu mulai dihitung sejak setiap soal tampil di layar dan berhenti dihitung saat soal dijawab.")
 cat("===================================================\n")
-cat("\n\n")
+cat("\n\nKetik:\nmulai()\n\nLalu enter untuk memulai.")
 
 mulai = function(){
   nama = readline(prompt = "Masukkan nama kamu: ")
