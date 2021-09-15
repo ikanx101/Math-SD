ba08735358cb84de36fa2c6965878bccbb1e89fd
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2021-09-15 17:30:19 +0700

    Update main.R

--- main.R
+++ main.R
@@ -6,4 +6,16 @@ library(ggplot2)
 data = data.frame(waktu = NA,
 		  benar = NA)
 
-cat("\014")
+source("soal.R")
+
+clc()
+
+cat("===================================================\n")
+cat("            GENERATOR SOAL BERHITUNG\n")
+cat("			 versi 1.0\n")
+cat("===================================================\n")
+cat("\n\n")
+
+mulai = function(){
+  nama = readline(prompt = "Masukkan nama kamu: ")
+}
