9d061da66284eaf46f1fedd3918e8f6ad10eb4b7
ikanx101 <ikanx101@gmail.com>
2021-10-01 15:02:43 +0000

    ganti struktur

--- main.R
+++ main.R
@@ -22,7 +22,7 @@ clc()
 cat("===================================================\n")
 cat("             KUMOR: KUMON ON R\n")
 cat("      Generator Soal Berhitung ala Kumon\n")
-cat("                versi 3.0\n")
+cat("                versi 4.0\n")
 cat("\nSelamat datang di program ini. Program ini bertujuan\nuntuk membuat soal hitung-hitungan untuk anak SD kelas 2 - 4.\n")
 cat("Kamu bisa menentukan berapa banyak soal penambahan, pengurangan,\nperkalian, dan pembagian yang akan dikerjakan.\n")
 cat("Waktu mulai dihitung sejak setiap soal tampil di layar dan\nberhenti dihitung saat soal dijawab.\n")
@@ -35,77 +35,116 @@ mulai = function(){
   nama = readline(prompt = "Masukkan nama kamu: ")
   clc()
   cat(paste0("Halo ",nama,",\n"))
-  cat("Ada empat kategori soal, yakni:\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n")
+  cat("Ada lima kategori soal, yakni:\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Mengurutkan bilangan\n\n")
   cat("Kamu bisa menentukan berapa soal yang akan\ndikerjakan di setiap bagian tersebut.\n\n")
 
   Sys.sleep(5)
   cat("Kamu hanya bisa mengisikan nilai lebih besar atau sama dengan 0. \n\n")
-  # input banyaknya soal perbagian
+  
+  # input banyaknya soal
+  # penjumlahan
   n_penjumlahan = readline(prompt = "Masukkan banyaknya soal PENJUMLAHAN yang mau kamu kerjakan: ")
   cat("\n")
+  # pengurangan
   n_pengurangan = readline(prompt = "Masukkan banyaknya soal PENGURANGAN yang mau kamu kerjakan: ")
   cat("\n")
+  # perkalian
   n_perkalian = readline(prompt = "Masukkan banyaknya soal PERKALIAN yang mau kamu kerjakan: ")
   cat("\n")
+  # pembagian
   n_pembagian = readline(prompt = "Masukkan banyaknya soal PEMBAGIAN yang mau kamu kerjakan: ")
   cat("\n")
+  # pengurutan
+  n_pengurut = readline(prompt = "Masukkan banyaknya soal MENGURUTKAN BILANGAN yang hendak kamu kerjakan: ")
+  cat("\n")
   
-  # kasih jeda dulu  
-  gimmick_1()
+  clc()
 
   # cek apakah yang diinput integer
   n_penjumlahan = as.numeric(n_penjumlahan)
   n_pengurangan = as.numeric(n_pengurangan)
   n_pembagian = as.numeric(n_pembagian)
   n_perkalian = as.numeric(n_perkalian)
-
+  n_pengurut = as.numeric(n_pengurut)
+  
   # kita set jaring pengaman dulu
   if(is.na(n_penjumlahan) | n_penjumlahan <= 0) {
-     n_penjumlahan = 1
-     cat("Karena input banyaknya soal penjumlahan salah, maka soalnya hanya ada 1.\n")
-     Sys.sleep(4)
+     n_penjumlahan = 0
+     cat("Tidak ada soal penjumlahan yang akan dikerjakan.\n")
+     Sys.sleep(2)
      }
   
   if(is.na(n_pengurangan) | n_pengurangan <= 0){
-     n_pengurangan = 1
-     cat("Karena input banyaknya soal pengurangan salah, maka soalnya hanya ada 1.\n")
-     Sys.sleep(4)
+     n_pengurangan = 0
+     cat("Tidak ada soal pengurangan yang akan dikerjakan.\n")
+     Sys.sleep(2)
      }
   
   if(is.na(n_perkalian) | n_perkalian <= 0){
-     n_perkalian = 1
-     cat("Karena input banyaknya soal perkalian salah, maka soalnya hanya ada 1.\n") 
-     Sys.sleep(4)
+     n_perkalian = 0
+     cat("Tidak ada soal perkalian yang akan dikerjakan.\n") 
+     Sys.sleep(2)
      }
 
   if(is.na(n_pembagian) | n_pembagian <= 0){
-     n_pembagian = 1
-     cat("Karena input banyaknya soal pembagian salah, maka soalnya hanya ada 1.\n")
-     Sys.sleep(4)
+     n_pembagian = 0
+     cat("Tidak ada soal pembagian yang akan dikerjakan.\n")
+     Sys.sleep(2)
      }
   
+  if(is.na(n_pengurut) | n_pengurut <= 0){
+     n_pengurut = 0
+     cat("Tidak ada soal mengurutkan yang akan dikerjakan.\n\n")
+     Sys.sleep(4)
+     }
+
+  # seandainya tidak ada soal sama sekali yang dikerjakan
+  # kita akan paksa agar stop
+  # set marker dulu
+  marker_1 = sum(n_penjumlahan,n_pengurangan,n_perkalian,n_pembagian,
+                 n_pengurut)
+  # proses stop
+  if(marker_1 == 0){
+    cat("TIDAK ADA SOAL SAMA SEKALI YANG DIKERJAKAN\n\n")
+    stop("ketik mulai() untuk mengulangi dari awal.")
+    } else {
+      # set gimmick dulu
+      gimmick_1()
+      }
+
   # bersiap untuk memulai
   hitung_mundur()
 
   # kita mulai penjumlahan dulu
-  for(i in 1:n_penjumlahan){
+  dummy = 1
+  while(dummy <= n_penjumlahan){
     rekap[ikang,] = penjumlahan()
     ikang = ikang + 1
+    dummy = dummy + 1
     }
+  
   # loop pengurangan
-  for(i in 1:n_pengurangan){
+  dummy = 1
+  while(dummy <= n_pengurangan){
     rekap[ikang,] = pengurangan()
     ikang = ikang + 1
+    dummy = dummy + 1
     }
+
   # loop perkalian
-  for(i in 1:n_perkalian){
+  dummy = 1
+  while(dummy <= n_perkalian){
     rekap[ikang,] = perkalian()
     ikang = ikang + 1
+    dummy = dummy + 1
     }
+
   # loop pembagian
-  for(i in 1:n_pembagian){
+  dummy = 1
+  while(dummy <= n_pembagian){
     rekap[ikang,] = pembagian()
     ikang = ikang + 1
+    dummy = dummy + 1
     }
 
   # kasih jeda biar terlihat dia benar atau tidak 
@@ -136,7 +175,7 @@ mulai = function(){
 		   "Rata-rata, kamu membutuhkan ",
 		   waktu_rata,
 		   " detik\nuntuk menjawab satu soal.\n\n")
-  pesan_3 = paste0("Kamu berhasil menjawab ",
+  pesan_3 = paste0("Kamu menjawab ",
 		   benar_total,
 		   " buah soal dengan benar\ndari total ",
 		   total_soal,
